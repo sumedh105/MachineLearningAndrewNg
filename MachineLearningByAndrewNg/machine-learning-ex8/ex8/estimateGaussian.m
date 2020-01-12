9e9a87d56685506519a56d4683af3f45d180c5ec
sumedh105@gmail.com <Passwordschumi@105>
2020-01-12 20:05:07 +0100

    Anomaly detection

---
+++ MachineLearningByAndrewNg/machine-learning-ex8/ex8/estimateGaussian.m
@@ -0,0 +1,49 @@
+function [mu sigma2] = estimateGaussian(X)
+%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%Gaussian distribution using the data in X
+%   [mu sigma2] = estimateGaussian(X), 
+%   The input X is the dataset with each n-dimensional data point in one row
+%   The output is an n-dimensional vector mu, the mean of the data set
+%   and the variances sigma^2, an n x 1 vector
+% 
+
+% Useful variables
+[m, n] = size(X);
+
+% You should return these values correctly
+mu = zeros(n, 1);
+sigma2 = zeros(n, 1);
+
+sizeOfX = size(X);			% This is a debug comment: 307x2
+valueOfM = m;				% This is a debug comment: 307
+valueOfN = n;				% This is a debug comment: 2
+sizeOfSigma2 = size(sigma2);		% This is a debug comment: 2x1
+sizeOfMu = size(mu);			% This is a debug comment: 2x1
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the mean of the data and the variances
+%               In particular, mu(i) should contain the mean of
+%               the data for the i-th feature and sigma2(i)
+%               should contain variance of the i-th feature.
+%
+
+for i = 1:n,
+	for j = 1:m,
+		mu(i) = mu(i) + X(j, i);
+	end
+
+	mu(i) = (mu(i) / m);
+end
+
+for i = 1:n,
+	for j = 1:m,
+		sigma2(i) = sigma2(i) + ((X(j, i) - mu(i)) ^ 2);
+	end
+
+	sigma2(i) = (sigma2(i) / m);
+end
+
+% =============================================================
+
+
+end
